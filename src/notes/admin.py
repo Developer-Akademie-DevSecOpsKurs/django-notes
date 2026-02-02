77f8bc0b785a032a83aa10514de479700816ff06
Sven Patrick Meier <sven.patrick.meier@gmail.com>
2026-02-02 14:03:18 +0100

    feat: add notes to admin panel

--- src/notes/admin.py
+++ src/notes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Notes, NotesUser
+
+
+class NotesUserAdminPanel(admin.ModelAdmin):
+
+    list_display = ("username", "is_staff", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at")
+    # readonly_fields = ("date_joined",)
+
+
 # Register your models here.
+admin.site.register(Notes)
+admin.site.register(NotesUser, NotesUserAdminPanel)
