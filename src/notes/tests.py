0cb0110218921dadb55e369b091e59477dc8554b
Sven Patrick Meier <sven.patrick.meier@gmail.com>
2026-03-19 20:39:25 +0100

    chore: format import sorting

--- src/notes/tests.py
+++ src/notes/tests.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.test import TestCase, Client
+from django.test import Client, TestCase
 from django.urls import reverse
 
 from .models import Note, NotesUser
